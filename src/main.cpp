202bc8bcf0e1e965ce693966e7203f67ab395383
joffr <joffreyestrella2010@gmail.com>
2025-09-05 19:16:01 +0100

    Replaced unnecessary while loop in updateWeather function

--- src/main.cpp
+++ src/main.cpp
@@ -15,14 +15,14 @@
 
 #define BUTTON 33
 
-const char *ssid = "SSID";
-const char *password = "password";
+const char *ssid = "VM8467663";
+const char *password = "vcV3mqhZse7ctooc";
 
 // NOTE : Set time zone here (https://timeapi.io/api/timezone/availabletimezones)
 String timeApi = "https://timeapi.io/api/time/current/zone?timeZone=Europe%2FLondon";
 
 // NOTE : Set lat, lon and API key here (https://home.openweathermap.org/api_keys)
-const char *weatherApi = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}&units=metric";
+const char *weatherApi = "https://api.openweathermap.org/data/2.5/weather?lat=51.653625&lon=-0.27329490&appid=e26d432445d40721d0a2844ef048feea&units=metric";
 
 struct Alarm {
   bool silenced;
@@ -38,8 +38,8 @@ enum Page {
 };
  
 std::vector<Alarm> alarms = {
-  Alarm{ .hour = 7, .name = "WAKE UP WAKE UP WAKE UP" },
-  Alarm{ .hour = 22, .minute = 30, .name = "Bedtime" }
+  Alarm{ .weekends = false, .hour = 7, .name = "WAKE UP WAKE UP WAKE UP" },
+  Alarm{ .weekends = true, .hour = 22, .minute = 30, .name = "Bedtime" }
 };
 Alarm *currentAlarm = nullptr;
 Page page = TIME;
@@ -161,8 +161,9 @@ void loop() {
 
 void updateTime() {
   Serial.println("Fetching time");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Failed to update time: no connection");
+    return;
   }
 
   HTTPClient http;
@@ -413,6 +414,8 @@ void checkAlarms() {
       continue;
     }
     if (alarm.hour != hour || alarm.minute != minute)
+      continue;
+    if (!alarm.weekends && (dayOfWeek == "Sunday" || dayOfWeek == "Saturday"))
       continue;
     currentAlarm = &alarm;
     resetDisplayAlarm();
